121fcfc8d1df4fdacae54e183c56af3ce97af596
Anoop <48412204+Anoop684@users.noreply.github.com>
2022-03-07 00:12:45 +0530

    Add files via upload

--- Python_MCAS1/leap_year.py
+++ Python_MCAS1/leap_year.py
@@ -3,6 +3,7 @@ e_year=int(input("Enter the ending year:"))
 
 print("The Leap years from ",s_year," to ",e_year," are : ")
 for i in range(s_year,e_year+1):
+    # print(i)
     if((i%4==0 and i%100!=0) or (i%400 == 0)):
         print(i,end=" ")
     
