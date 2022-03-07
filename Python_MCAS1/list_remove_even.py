cc58a22fccff1f754f93c5a955628c899a8bb46c
Anoop <48412204+Anoop684@users.noreply.github.com>
2022-03-07 07:47:17 +0530

    Add files via upload

--- Python_MCAS1/list_remove_even.py
+++ Python_MCAS1/list_remove_even.py
@@ -1,20 +1,10 @@
-def list_reven(l):
-
-    l2=[]
-
-    for i in l:
-        if(i%2!=0):
-            l2.append(i)
-
-    return l2
+from re import L
 
 
-
-n=input("Enter integers space separated")
-list1=list(map(int,n.split()))
-print("list before removing even integers: ",list1)
-list2=list_reven(list1)
-print("list after removing even integers: ",list2)
-
+l=[11,22,33,44,55]
+for i in l:
+    if(i%2==0):
+        l.remove(i)
+print(l)
 
            
